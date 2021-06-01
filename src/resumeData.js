47d5cc353c140b554b2e7e917deadb34c6d40169
Tridiv Kumar Rabha <tridivkumar@gmail.com>
2021-06-01 19:31:24 +0530

    all components are added

---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    //"imagebaseurl":"https://github.com/iamtridiv",
+    "name": "Tridiv",
+    "role": "currently pursuing my master's degree in NIT Karnataka",
+    "linkedinId":"https://www.linkedin.com/in/tridiv-kumar-rabha-ab018510a/",
+    "skypeid": "Your skypeid",
+    "roleDescription": " Apart from study stuff, I love cycling, swimming, practicing martial arts.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/tridiv-kumar-rabha-ab018510a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/iamtridiv",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/iamtridiv",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a final year student at the National Institute of Technology, Karnataka. I got good grasping capability and super passionate to learn new technologies to keep myself updated.",
+    "address":"Guwahati, Assam, India",
+    "website":"https://www.github.com/iamtridiv",
+    "education":[
+      {
+        "UniversityName":"National Institute of Technology, Karnatka",
+        "specialization":"Compouter Science & Information Security",
+        "MonthOfPassing":"Present",
+        "YearOfPassing":"2022"
+      },
+      {
+        "UniversityName":"Jorhat Engineering College",
+        "specialization":"Computer Science & Engineering",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2018"
+        
+      }
+    ],
+    "Projects":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
